cab8ae89d8d2c24adfe23b63620c8545dc06bd17
Zoe Ames <zoeames@gmail.com>
2019-04-22 19:28:54 -0500

    locations finished

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -5,11 +5,32 @@ import './locations.scss';
 
 let locations = [];
 
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-success';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-info';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+      selectedClass = '';
+  }
+  return selectedClass;
+};
+
 const domStringBuilder = () => {
   let domString = '';
   locations.forEach((location) => {
     domString += `<div id=${location.id} class="card location col-2">`;
-    domString += `<div class="card-header">${location.name}</div>`;
+    domString += `<div class="card-header ${shootTimeClass(location.shootTime)}">${location.name}</div>`;
     domString += '<div class="card-body">';
     domString += `<img class="card-img-top" src="${location.imageUrl}" alt="${location.name}">`;
     domString += `<h5 class="card-title">${location.address}</h5>`;
